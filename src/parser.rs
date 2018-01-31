f6300a09d112810aa05235fd65f9edae14780f5c
Jupp Müller <jupp0r@gmail.com>
2018-01-31 15:04:42 -0800

    Parse arguments

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,3 @@
-use errors::*;
 use nom::{is_alphanumeric, is_space};
 
 named!(
@@ -51,27 +50,32 @@ pub struct Argument {
 }
 
 named!(method(&[u8]) -> Method,
-       dbg_dmp!(preceded!(take_until_and_consume!("virtual"), dbg_dmp!(terminated!(method_inner, take_until_and_consume!(";"))))));
+       dbg_dmp!(delimited!(take_until_and_consume!("virtual"), method_inner, take_until_and_consume!(";"))));
 named!(method_inner(&[u8]) -> Method,
        dbg_dmp!(ws!(map!(
-           tuple!(
+           ws!(tuple!(
                take_while!(is_alphanumeric),
-               ws!(take_while!(is_alphanumeric)),
-               preceded!(
-                   take_until_and_consume!("("),
+               take_until!("("),
+           dbg_dmp!(delimited!(tag!("("),
                    separated_list!(
-                       tag!(","),
-                       parse_argument)
-               )
-           ), make_method))));
+                       char!(','),
+                       parse_argument), preceded!(tag!(")"), take_until!(";"))))))
+           , make_method))));
 
 named!(parse_argument(&[u8]) -> Argument,
-       map!(ws!(separated_list!(
+       dbg_dmp!(map!(preceded!(many0!(tag!(" ")), separated_list!(
            take_while!(is_space),
            take_while!(is_alphanumeric)))
-            , make_argument));
+            , make_argument)));
 
 fn make_argument(parsed_argument: Vec<&[u8]>) -> Argument {
+    println!(
+        "{:?}",
+        parsed_argument
+            .iter()
+            .map(|v| String::from_utf8(v.to_vec()).unwrap())
+            .collect::<Vec<String>>()
+    );
     Argument {
         argument_type: String::from_utf8(parsed_argument[0].to_owned()).unwrap(),
         argument_name: Some(String::from_utf8(parsed_argument[1].to_owned()).unwrap()),
@@ -215,5 +219,39 @@ namespace baz{
         let simple_method = &b"virtual void foobar() = 0;"[..];
         let parsed_method = method(simple_method).to_result().unwrap();
         assert_eq!(parsed_method.name, "foobar".to_owned());
+    }
+
+    #[test]
+    fn test_simple_method_arguments() {
+        let simple_method = &b"virtual void foobar() = 0;"[..];
+        let parsed_method = method(simple_method).to_result().unwrap();
+        assert_eq!(parsed_method.arguments.len(), 0);
+    }
+
+    #[test]
+    fn test_method_with_one_argument() {
+        let method_with_arguments = &b"virtual void baz(int abc) = 0;"[..];
+        let parsed_method = method(method_with_arguments).to_result().unwrap();
+        assert_eq!(parsed_method.arguments[0].argument_type, "int".to_owned());
+        assert_eq!(
+            parsed_method.arguments[0].argument_name,
+            Some("abc".to_owned())
+        );
+    }
+
+    #[test]
+    fn test_method_with_two_arguments() {
+        let method_with_arguments = &b"virtual void baz(int abc, bool cde) = 0;"[..];
+        let parsed_method = method(method_with_arguments).to_result().unwrap();
+        assert_eq!(parsed_method.arguments[0].argument_type, "int".to_owned());
+        assert_eq!(
+            parsed_method.arguments[0].argument_name,
+            Some("abc".to_owned())
+        );
+        assert_eq!(parsed_method.arguments[1].argument_type, "bool".to_owned());
+        assert_eq!(
+            parsed_method.arguments[1].argument_name,
+            Some("cde".to_owned())
+        );
     }
 }
